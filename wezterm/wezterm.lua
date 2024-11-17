e4f285cd86dffa12860fe6e769a9cebce8e6da59
Baptiste GAZE <bpt.gaze@gmail.com>
2024-11-17 22:01:13 +0100

    update wezterm cfg

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,12 +7,34 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+config.initial_rows = 30
+config.initial_cols = 120
+
 -- Font
-config.font = wezterm.font("Iosevka Term", { weight = "Bold" })
+config.font = wezterm.font("Iosevka Term", {
+	-- stretch = "Expanded",
+	weight = "Bold",
+})
 config.font_size = 15.0
 config.color_scheme = "Catppuccin Frappe"
-config.window_background_opacity = 0.98
-config.macos_window_background_blur = 10
+config.bold_brightens_ansi_colors = true
+
+-- System
+config.audible_bell = "Disabled"
+config.front_end = "WebGpu"
+config.max_fps = 120
+config.animation_fps = 10
+config.use_ime = false
+
+-- window
+config.window_background_opacity = 0.97
+config.macos_window_background_blur = 30
+config.window_padding = {
+	left = 2,
+	right = 2,
+	top = 0,
+	bottom = 0,
+}
 
 -- Use kitty protocol
 config.enable_kitty_keyboard = true
@@ -24,18 +46,30 @@ config.window_decorations = "RESIZE"
 -- Cursor
 config.default_cursor_style = "BlinkingBar"
 
+-- tabbar
+config.enable_tab_bar = true
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_max_width = 80
+config.tab_and_split_indices_are_zero_based = true
+
+-- system
+config.term = "xterm-256color"
+
 -- Special key mapping for nvim
 -- From: https://github.com/wez/wezterm/issues/3731#issuecomment-1592198263
 local function is_vim(pane)
-	local is_vim_env = pane:get_user_vars().IS_NVIM == "true"
-	if is_vim_env == true then
-		return true
-	end
-	-- This gsub is equivalent to POSIX basename(3)
-	-- Given "/foo/bar" returns "bar"
-	-- Given "c:\\foo\\bar" returns "bar"
-	local process_name = string.gsub(pane:get_foreground_process_name(), "(.*[/\\])(.*)", "%2")
-	return process_name == "nvim" or process_name == "vim"
+	return (pane:get_foreground_process_name() or ""):find("n?vim") ~= nil
+	-- local is_vim_env = pane:get_user_vars().IS_NVIM == "true"
+	-- if is_vim_env == true then
+	-- 	return true
+	-- end
+	-- -- This gsub is equivalent to POSIX basename(3)
+	-- -- Given "/foo/bar" returns "bar"
+	-- -- Given "c:\\foo\\bar" returns "bar"
+	-- local process_name = string.gsub(pane:get_foreground_process_name(), "(.*[/\\])(.*)", "%2")
+	-- return process_name == "nvim" or process_name == "vim"
 end
 
 -- cmd+keys that we want to send to neovim.
@@ -95,8 +129,18 @@ config.keys = {
 		action = wezterm.action.SendString("\x1bf"),
 	},
 	{
+		key = "RightArrow",
+		mods = "CMD",
+		action = wezterm.action.SendString("\x05"),
+	},
+	{
+		key = "LeftArrow",
+		mods = "CMD",
+		action = wezterm.action.SendString("\x01"),
+	},
+	{
 		key = "i",
-		mods = "CMD|OPT",
+		mods = "CMD|SHIFT",
 		action = act.PromptInputLine({
 			description = "Enter new name for tab",
 			-- initial_value = "My Tab Name",
@@ -110,20 +154,41 @@ config.keys = {
 			end),
 		}),
 	},
-}
-
--- tab bar
-config.tab_bar_at_bottom = true
-config.use_fancy_tab_bar = false
-
--- prevents addition of padding at bottom
--- config.use_resize_increments = true
-
-config.window_padding = {
-	left = 2,
-	right = 2,
-	top = 0,
-	bottom = 0,
+	-- Create a new tab in the same domain as the current pane.
+	{
+		key = "t",
+		mods = "CMD",
+		action = act({ SpawnCommandInNewTab = { cwd = wezterm.home_dir } }),
+	},
+	-- Create a new tab in the default domain
+	{ key = "t", mods = "SHIFT|CMD", action = act.SpawnTab("CurrentPaneDomain") },
+	-- Toggle fullscreen
+	{
+		key = "Enter",
+		mods = "CMD|OPT",
+		action = wezterm.action.ToggleFullScreen,
+	},
+	-- Move between tabs
+	{
+		key = "h",
+		mods = "CMD|SHIFT",
+		action = act.ActivateTabRelative(-1),
+	},
+	{
+		key = "l",
+		mods = "CMD|SHIFT",
+		action = act.ActivateTabRelative(1),
+	},
+	{
+		key = "LeftArrow",
+		mods = "CMD|SHIFT",
+		action = act.MoveTabRelative(-1),
+	},
+	{
+		key = "RightArrow",
+		mods = "CMD|SHIFT",
+		action = act.MoveTabRelative(1),
+	},
 }
 
 -- and finally, return the configuration to wezterm
